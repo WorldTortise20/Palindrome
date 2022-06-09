6d6538a724d9148fe2c8a276b1a2209d0f9e1ff6
WorldTortise20 <72114522+WorldTortise20@users.noreply.github.com>
2022-06-08 20:44:58 -0700

    6/8/2022

--- automation.py
+++ automation.py
@@ -1,6 +1,35 @@
-a = "Users\vaibh\Desktop\WhiteHat VS Code\pythonProgramming\countingProgram"
-counter = 0
-for slash in a:
-        counter = counter + 1
+#Code to see if a certain word is a palindrome or not
 
-print(counter)
+charCount = 0
+counter = 1
+newCounter = 0
+word = input("Type any word and see if it's a palindrome!")
+backwordsList = []
+valid = "true"
+
+
+for i in word:
+    charCount = charCount+1
+    newCharCount = charCount
+    if(i == " "):
+        valid = "false"
+        
+letterSplit = list(word.lower())
+
+#print(letterSplit)
+for a in letterSplit:
+    if(newCharCount>0):
+        backwords = letterSplit[charCount-counter]
+        backwordsList.append(backwords)
+        newCharCount -=1
+        counter = counter + 1
+        
+#print(backwordsList)
+
+if(valid == "true"):
+    if(letterSplit == backwordsList):
+        print("Palindrome")
+    else:
+        print("Not a Palindrome")
+else:
+    print("Not a word")
